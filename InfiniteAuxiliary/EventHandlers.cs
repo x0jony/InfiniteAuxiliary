5ec6733279d207a81ce91326707ef91146587c8f
x0jony <73110140+x0jony@users.noreply.github.com>
2024-07-26 01:55:42 +0300

    Fixed infinite auxiliary power

--- InfiniteAuxiliary/EventHandlers.cs
+++ InfiniteAuxiliary/EventHandlers.cs
@@ -1,5 +1,6 @@
 ﻿using Exiled.API.Features.Roles;
 using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp079;
 using PlayerRoles;
 
 namespace InfiniteAuxiliary
@@ -11,10 +12,30 @@ namespace InfiniteAuxiliary
 
         internal void OnSpawning(SpawningEventArgs ev)
         {
-            if (ev.Player.Role != RoleTypeId.Scp079) return;
+            if (ev.Player.Role.Type != RoleTypeId.Scp079) return;
             
             Scp079Role scp079 = ev.Player.Role.As<Scp079Role>();
             scp079.AuxManager.CurrentAux = float.MaxValue;
         }
+        
+        internal void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (ev.Player.Role.Type != RoleTypeId.Scp079) return;
+            
+            Scp079Role scp079 = ev.Player.Role.As<Scp079Role>();
+            scp079.AuxManager.CurrentAux = float.MaxValue;
+        }
+        
+        internal void OnPinging(PingingEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnChangingCamera(ChangingCameraEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnElevatorTeleporting(ElevatorTeleportingEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnGainingExperience(GainingExperienceEventArgs ev) { ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnGainingLevel(GainingLevelEventArgs ev) { ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnInteractingTesla(InteractingTeslaEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnLockingDown(LockingDownEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnRoomBlackout(RoomBlackoutEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnTriggeringDoor(TriggeringDoorEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnZoneBlackout(ZoneBlackoutEventArgs ev) { ev.AuxiliaryPowerCost = 0; ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
+        internal void OnChangingSpeakerStatus(ChangingSpeakerStatusEventArgs ev) { ev.Scp079.AuxManager.CurrentAux = float.MaxValue; }
     }
 }
--- InfiniteAuxiliary/Methods.cs
+++ InfiniteAuxiliary/Methods.cs
@@ -10,10 +10,36 @@
             if (enabled)
             {
                 Exiled.Events.Handlers.Player.Spawning += plugin.EventHandlers.OnSpawning;
+                Exiled.Events.Handlers.Player.ChangingRole += plugin.EventHandlers.OnChangingRole;
+                
+                Exiled.Events.Handlers.Scp079.Pinging += plugin.EventHandlers.OnPinging;
+                Exiled.Events.Handlers.Scp079.ChangingCamera += plugin.EventHandlers.OnChangingCamera;
+                Exiled.Events.Handlers.Scp079.ElevatorTeleporting += plugin.EventHandlers.OnElevatorTeleporting;
+                Exiled.Events.Handlers.Scp079.GainingExperience += plugin.EventHandlers.OnGainingExperience;
+                Exiled.Events.Handlers.Scp079.GainingLevel += plugin.EventHandlers.OnGainingLevel;
+                Exiled.Events.Handlers.Scp079.InteractingTesla += plugin.EventHandlers.OnInteractingTesla;
+                Exiled.Events.Handlers.Scp079.LockingDown += plugin.EventHandlers.OnLockingDown;
+                Exiled.Events.Handlers.Scp079.RoomBlackout += plugin.EventHandlers.OnRoomBlackout;
+                Exiled.Events.Handlers.Scp079.TriggeringDoor += plugin.EventHandlers.OnTriggeringDoor;
+                Exiled.Events.Handlers.Scp079.ZoneBlackout += plugin.EventHandlers.OnZoneBlackout;
+                Exiled.Events.Handlers.Scp079.ChangingSpeakerStatus += plugin.EventHandlers.OnChangingSpeakerStatus;
             }
             else
             {
                 Exiled.Events.Handlers.Player.Spawning -= plugin.EventHandlers.OnSpawning;
+                Exiled.Events.Handlers.Player.ChangingRole -= plugin.EventHandlers.OnChangingRole;
+                
+                Exiled.Events.Handlers.Scp079.Pinging -= plugin.EventHandlers.OnPinging;
+                Exiled.Events.Handlers.Scp079.ChangingCamera -= plugin.EventHandlers.OnChangingCamera;
+                Exiled.Events.Handlers.Scp079.ElevatorTeleporting -= plugin.EventHandlers.OnElevatorTeleporting;
+                Exiled.Events.Handlers.Scp079.GainingExperience -= plugin.EventHandlers.OnGainingExperience;
+                Exiled.Events.Handlers.Scp079.GainingLevel -= plugin.EventHandlers.OnGainingLevel;
+                Exiled.Events.Handlers.Scp079.InteractingTesla -= plugin.EventHandlers.OnInteractingTesla;
+                Exiled.Events.Handlers.Scp079.LockingDown -= plugin.EventHandlers.OnLockingDown;
+                Exiled.Events.Handlers.Scp079.RoomBlackout -= plugin.EventHandlers.OnRoomBlackout;
+                Exiled.Events.Handlers.Scp079.TriggeringDoor -= plugin.EventHandlers.OnTriggeringDoor;
+                Exiled.Events.Handlers.Scp079.ZoneBlackout -= plugin.EventHandlers.OnZoneBlackout;
+                Exiled.Events.Handlers.Scp079.ChangingSpeakerStatus -= plugin.EventHandlers.OnChangingSpeakerStatus;
             }
         }
     }
